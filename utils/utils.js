381613431063153415f3909196bc74f7e270b188
binuka97 <Binukakamesh97@gmail.com>
2022-01-17 21:01:07 +0530

    linked pages

--- utils/utils.js
+++ utils/utils.js
@@ -1,3 +1,11 @@
 function checkoutPage() {
     window.location.assign("../../Pages/Checkout/Checkout.html")
 }
+
+function cartPage() {
+    window.location.assign("../../Pages/Cart/Cart.html")
+}
+
+function favPage() {
+    window.location.assign("../../Pages/favourites/favourites.html")
+}
